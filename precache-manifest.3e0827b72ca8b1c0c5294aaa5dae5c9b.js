3555571f70f1751be41096463ef5612c6424b756
VodeniZeko <salexy.es@gmail.com>
2019-07-11 21:44:15 -0700

    Updates

---
+++ precache-manifest.3e0827b72ca8b1c0c5294aaa5dae5c9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6bc5a0a3677c1b4df82ed60ea088d3",
+    "url": "/googly/index.html"
+  },
+  {
+    "revision": "92495658e66c81b75bd3",
+    "url": "/googly/static/css/main.c238194e.chunk.css"
+  },
+  {
+    "revision": "0201679974ebd31c2a5b",
+    "url": "/googly/static/js/2.1bb3f896.chunk.js"
+  },
+  {
+    "revision": "92495658e66c81b75bd3",
+    "url": "/googly/static/js/main.16ab2758.chunk.js"
+  },
+  {
+    "revision": "af24e6d332e8bc00f10e",
+    "url": "/googly/static/js/runtime~main.5cce9e5d.js"
+  }
+]);
